fcba084547dcdd78d45b2cce66e230dc821a648a
freedom_lovver <maksbuharskij16@gmail.com>
2025-10-12 00:25:24 +0500

    feat(dto): create LoginRequest and RegisterRequest DTO. Contains email and password. Add JWT dependencies into Gradle.

---
+++ src/main/java/com/shortlinkv1/ShortLink/dto/LoginRequest.java
@@ -0,0 +1,8 @@
+package com.shortlinkv1.ShortLink.dto;
+
+public class LoginRequest {
+
+    public String email;
+    public String password;
+
+}
---
+++ src/main/java/com/shortlinkv1/ShortLink/dto/RegisterRequest.java
@@ -0,0 +1,8 @@
+package com.shortlinkv1.ShortLink.dto;
+
+public class RegisterRequest {
+
+    public String email;
+    public String password;
+
+}

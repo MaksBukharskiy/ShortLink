b67d7e105695275c6f7cbf6945e2b9ebb99c8339
freedom_lovver <maksbuharskij16@gmail.com>
2025-10-12 00:34:13 +0500

    feat(auth): add AuthRestController with POST /api/auth/register endpoint. Implemented request handling via RegisterRequest DTO. Integrated with UserService for registration flow. Returns 201 Created on success. Also create /api/auth/register endpoint, which return JWT token.

--- src/main/java/com/shortlinkv1/ShortLink/controller/AuthRestController.java
+++ src/main/java/com/shortlinkv1/ShortLink/controller/AuthRestController.java
@@ -1,10 +1,13 @@
 package com.shortlinkv1.ShortLink.controller;
 
+import com.shortlinkv1.ShortLink.dto.LoginRequest;
+import com.shortlinkv1.ShortLink.dto.RegisterRequest;
+import com.shortlinkv1.ShortLink.repository.UserRepository;
+import com.shortlinkv1.ShortLink.security.JwtTokenProvider;
 import com.shortlinkv1.ShortLink.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,23 +15,35 @@ import org.springframework.web.bind.annotation.*;
 public class AuthRestController {
 
     private final UserService userService;
+    private final JwtTokenProvider jwtToken;
 
     @Autowired
-    public AuthRestController(UserService userService) {
+    public AuthRestController(UserService userService,  JwtTokenProvider jwtToken,  UserRepository userRepository) {
         this.userService = userService;
+        this.jwtToken = jwtToken;
     }
-
-    public static class RegisterRequest {
-        public String email;
-        public String password;
-    }
-
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public void registerUser(@Valid @RequestBody RegisterRequest registerRequest) {
-        userService.register(registerRequest.email,  registerRequest.password);
+        userService.register(registerRequest.email, registerRequest.password);
     }
 
-}
 
+    @PostMapping("/login")
+    public String login(@Valid @RequestBody LoginRequest loginRequest) {
+
+        var user = userService.findByEmail(loginRequest.email)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Email"));
+
+
+         if (!userService.checkPassword(loginRequest.password, user.getPasswordHash())){
+             throw new IllegalArgumentException("Invalid Password");
+         }
+
+         return  jwtToken.generateToken(user.getEmail());
+
+    }
+
+
+}

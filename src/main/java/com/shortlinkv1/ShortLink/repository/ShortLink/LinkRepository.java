1e127f33819b77e6f92434f6470a4862f4234f30
freedom_lovver <maksbuharskij16@gmail.com>
2025-10-14 17:50:54 +0500

    feat(link): implement Link repository with Data JPA.
    Add findByShortCode method for efficient link lookups.

---
+++ src/main/java/com/shortlinkv1/ShortLink/repository/ShortLink/LinkRepository.java
@@ -0,0 +1,10 @@
+package com.shortlinkv1.ShortLink.repository.ShortLink;
+
+import com.shortlinkv1.ShortLink.entity.ShortLink;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LinkRepository extends JpaRepository<ShortLink, Long> {
+    Optional<ShortLink> findByShortCode(String shortCode);
+}

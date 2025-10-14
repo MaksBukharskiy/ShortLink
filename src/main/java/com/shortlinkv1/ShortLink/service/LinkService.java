0c8227b187a869718a5306a8ac5b213e0471ca87
freedom_lovver <maksbuharskij16@gmail.com>
2025-10-14 17:48:58 +0500

    feat(link): implement short link generation service
    Add service for creating and managing short links.
    Features unique code generation with collision handling.
    Transactional creation with user association.
    Secure random code generation for URL shortening.

---
+++ src/main/java/com/shortlinkv1/ShortLink/service/LinkService.java
@@ -0,0 +1,54 @@
+package com.shortlinkv1.ShortLink.service;
+
+import com.shortlinkv1.ShortLink.entity.ShortLink;
+import com.shortlinkv1.ShortLink.entity.UserEntity.User;
+import com.shortlinkv1.ShortLink.repository.ShortLink.LinkRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+
+@Service
+public class LinkService {
+
+    private final LinkRepository linkRepository;
+    private static final int CODE_LENGTH = 8;
+    private static final String CHARACTERS = "0123456789ABCDEFGHIJ";
+    private static final SecureRandom random = new SecureRandom();
+
+    private String shortCode;
+
+
+    @Autowired
+    public LinkService(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    private String generateShortCode() {
+        StringBuilder sb = new StringBuilder(CODE_LENGTH);
+
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+
+    @Transactional
+    public ShortLink createShortlink(String originalUrl, User user) {
+
+        do {
+            shortCode = generateShortCode();
+        } while (linkRepository.findByShortCode(shortCode).isPresent());
+
+        ShortLink link = new ShortLink(originalUrl, shortCode, user);
+        return linkRepository.save(link);
+
+    }
+
+    public ShortLink findByShortCode(String shortCode) {
+        return linkRepository.findByShortCode(shortCode).
+                orElse(null);
+    }
+
+}

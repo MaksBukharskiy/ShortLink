32cbdae98aa096676b6228da1883451837f0b6ac
freedom_lovver <maksbuharskij16@gmail.com>
2025-11-02 22:50:20 +0500

    feat(qr): implement QR code generation service with ZXing - Create QrCodeService to generate PNG images from URLs using ZXing library - Use ByteArrayOutputStream and MatrixToImageWriter for in-memory image creation - Return byte[] for direct use in HTTP response - Enables QR functionality without file I/O

--- src/main/java/com/shortlinkv1/Backend/service/qrCode/QrCodeService.java
+++ src/main/java/com/shortlinkv1/Backend/service/qrCode/QrCodeService.java
@@ -1,4 +1,31 @@
 package com.shortlinkv1.Backend.service.qrCode;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.WriteAbortedException;
+import java.io.Writer;
+
+@Service
 public class QrCodeService {
+
+    public byte[] generateQrCode(String text, int width, int height) throws IOException, WriterException {
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+
+        return pngOutputStream.toByteArray();
+
+    }
+
 }

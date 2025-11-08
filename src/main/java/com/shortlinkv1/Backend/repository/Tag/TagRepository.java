eee4ced88ad4d415ff672a75bfe4eac9f601cdab
freedom_lovver <maksbuharskij16@gmail.com>
2025-11-08 12:06:25 +0500

    feat(repo): add TagRepository with findByName method

--- src/main/java/com/shortlinkv1/Backend/repository/Tag/TagRepository.java
+++ src/main/java/com/shortlinkv1/Backend/repository/Tag/TagRepository.java
@@ -1,4 +1,10 @@
 package com.shortlinkv1.Backend.repository.Tag;
 
-public class TagRepository {
+import com.shortlinkv1.Backend.entity.tagEntity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
 }

6dbcd643a43ebb9b092009bcced6065b7dcad4bc
freedom_lovver <maksbuharskij16@gmail.com>
2025-11-13 22:18:05 +0500

    feat(service): add scheduled cleanup for expired links
    - Implement @Scheduled task to delete expired links
    - Use fixedRate of 1 hour for cleanup
    - Enable @EnableScheduling in main application

--- src/main/java/com/shortlinkv1/Backend/service/link/Clean/LinkCleanupService.java
+++ src/main/java/com/shortlinkv1/Backend/service/link/Clean/LinkCleanupService.java
@@ -1,4 +1,23 @@
 package com.shortlinkv1.Backend.service.link.Clean;
 
+import com.shortlinkv1.Backend.repository.ShortLink.LinkRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@Transactional
 public class LinkCleanupService {
+
+    @Autowired
+    private LinkRepository linkRepository;
+
+    @Scheduled(fixedRate = 3600000)
+    public void deleteExpiredLinksService(){
+        linkRepository.deleteByExpiresAtBefore(LocalDateTime.now());
+    }
+
 }

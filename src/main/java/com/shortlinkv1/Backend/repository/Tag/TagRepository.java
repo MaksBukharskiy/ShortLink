38b0e23b6799a3bda2d6c51ad0c3226a715349eb
freedom_lovver <maksbuharskij16@gmail.com>
2025-11-15 18:21:05 +0500

    feat(repo): add new method to TagRespository
    - Add List<Tag> .findAll, for returning all tags

--- src/main/java/com/shortlinkv1/Backend/repository/Tag/TagRepository.java
+++ src/main/java/com/shortlinkv1/Backend/repository/Tag/TagRepository.java
@@ -3,8 +3,12 @@ package com.shortlinkv1.Backend.repository.Tag;
 import com.shortlinkv1.Backend.entity.tagEntity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
     Optional<Tag> findByName(String name);
+
+    List<Tag> findAll();
 }

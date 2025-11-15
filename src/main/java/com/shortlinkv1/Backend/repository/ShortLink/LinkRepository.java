b4a8ae5713dedc4e4ea7ba6e1ca31c985eb3e35d
freedom_lovver <maksbuharskij16@gmail.com>
2025-11-15 18:20:27 +0500

    feat(repo): add new method to TagRespository
    - Add List<Tag> .findAll, for returning all tags

--- src/main/java/com/shortlinkv1/Backend/repository/ShortLink/LinkRepository.java
+++ src/main/java/com/shortlinkv1/Backend/repository/ShortLink/LinkRepository.java
@@ -1,6 +1,7 @@
 package com.shortlinkv1.Backend.repository.ShortLink;
 
 import com.shortlinkv1.Backend.entity.linkEntity.ShortLink;
+import com.shortlinkv1.Backend.entity.tagEntity.Tag;
 import com.shortlinkv1.Backend.entity.userEntity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -12,10 +13,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LinkRepository extends JpaRepository<ShortLink, Long> {
-    Optional<ShortLink> findByShortCode(String shortCode);
-    List<ShortLink> findByUser(User user);
 
     @Modifying
     @Query("DELETE FROM ShortLink s WHERE s.expiresAt IS NOT NULL AND s.expiresAt < :settime")
     void deleteByExpiresAtBefore(@Param("settime") LocalDateTime now);
+
+    Optional<ShortLink> findByShortCode(String shortCode);
+
+    List<ShortLink> findByUser(User user);
 }

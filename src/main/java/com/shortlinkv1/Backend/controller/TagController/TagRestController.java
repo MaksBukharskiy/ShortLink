220f2510dfbc3b9a8865125f4d7bff37be7d1f2f
freedom_lovver <maksbuharskij16@gmail.com>
2025-11-15 18:16:52 +0500

    feat(api): add endpoint to get all tags
    - Create GET /api/tags endpoint
    - Return list of all tag names

---
+++ src/main/java/com/shortlinkv1/Backend/controller/TagController/TagRestController.java
@@ -0,0 +1,32 @@
+package com.shortlinkv1.Backend.controller.TagController;
+
+import com.shortlinkv1.Backend.entity.tagEntity.Tag;
+import com.shortlinkv1.Backend.repository.Tag.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/tag")
+public class TagRestController {
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @GetMapping
+    public ResponseEntity <List<String>> getAll() {
+        List<Tag> tags = tagRepository.findAll();
+
+        List<String> tagNames = tags.stream()
+                .map(Tag::getName)
+                .toList();
+
+        return ResponseEntity.ok(tagNames);
+    }
+
+}

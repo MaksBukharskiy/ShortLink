6ad1a8355692768c5754667d16ef99535722f7aa
freedom_lovver <maksbuharskij16@gmail.com>
2025-11-21 15:56:40 +0500

    fix: update test
    - Gradle cannot buid project
    - Explicitly specified the class

--- src/test/java/com/shortlinkv1/ShortLink/ShortLinkApplicationTests.java
+++ src/test/java/com/shortlinkv1/ShortLink/ShortLinkApplicationTests.java
@@ -2,12 +2,12 @@ package com.shortlinkv1.ShortLink;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.shortlinkv1.Backend.ShortLinkApplication; // Импортируйте главный класс
 
-@SpringBootTest
+@SpringBootTest(classes = ShortLinkApplication.class) // Явно укажите класс
 class ShortLinkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }

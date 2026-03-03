ac925a119e49bd7b9f612a81c0a8bbc38846bcdd
freedom_lovver <maksbuharskij16@gmail.com>
2026-03-03 15:20:08 +0500

    Adjust security configuration to match current API access requirements.

--- src/main/java/com/shortlinkv1/Backend/config/rabbit/RabbitMqConfig.java
+++ src/main/java/com/shortlinkv1/Backend/config/rabbit/RabbitMqConfig.java
@@ -1,4 +1,17 @@
 package com.shortlinkv1.Backend.config.rabbit;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class RabbitMqConfig {
+
+    @Bean
+    public Queue testQueue(){
+        return new Queue("test.queue", true);
+
+    }
+
 }

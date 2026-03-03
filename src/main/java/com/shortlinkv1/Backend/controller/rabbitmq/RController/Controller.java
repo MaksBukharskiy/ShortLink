a8baae8ac912491f52f63a3a424357ab5e6b8247
freedom_lovver <maksbuharskij16@gmail.com>
2026-03-03 15:21:03 +0500

    Refactor RabbitMQ controller endpoints to match updated messaging flow.

--- src/main/java/com/shortlinkv1/Backend/controller/rabbitmq/RController/Controller.java
+++ src/main/java/com/shortlinkv1/Backend/controller/rabbitmq/RController/Controller.java
@@ -1,4 +1,25 @@
 package com.shortlinkv1.Backend.controller.rabbitmq.RController;
 
+import com.shortlinkv1.Backend.RabbitRoles.TestProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rabbitmq")
 public class Controller {
+
+    private final TestProducer testProducer;
+
+    @PostMapping("/test/request")
+    public void sendProduser(@RequestParam String msg){
+        testProducer.sendMessage(msg);
+        log.info("✅✅✅Producer✅✅✅");
+    }
+
 }

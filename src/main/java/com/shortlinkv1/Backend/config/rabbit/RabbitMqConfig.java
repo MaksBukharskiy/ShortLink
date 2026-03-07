5067c8124f31e26d46e1270c753a896d014efe65
freedom_lovver <maksbuharskij16@gmail.com>
2026-03-08 01:43:37 +0500

    Add RabbitMQ exchange and queue binding configuration. Created DirectExchange  and bound  using routing key . This enables message routing from producer to the appropriate queue for user creation events and asynchronous processing.

--- src/main/java/com/shortlinkv1/Backend/config/rabbit/RabbitMqConfig.java
+++ src/main/java/com/shortlinkv1/Backend/config/rabbit/RabbitMqConfig.java
@@ -1,5 +1,8 @@
 package com.shortlinkv1.Backend.config.rabbit;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,24 @@ public class RabbitMqConfig {
     public Queue testQueue(){
         return new Queue("test.queue", true);
 
+    }
+
+    @Bean
+    public Queue queueForInfo(){
+        return new Queue("queue.for.exchange", false);
+    }
+
+    @Bean
+    public DirectExchange userQueueExchange(){
+        return new DirectExchange("user.exchange");
+    }
+
+    @Bean
+    public Binding bindingTest(Queue queueForInfo, DirectExchange userQueueExchange){
+        return BindingBuilder
+                .bind(queueForInfo)
+                .to(userQueueExchange)
+                .with("binding.app");
     }
 
 }
